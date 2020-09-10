dc745e8de1ff331795b8c389d8fa950f6b5628b8
Gaurav Sachdev <gaurav1565@gmail.com>
2020-09-10 18:27:27 +0530

    THM Jacob user flag

---
+++ tryhackme/Jacob the Boss/feed.php
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?><feed xmlns="http://www.w3.org/2005/Atom" xml:lang="pt">
+	<title type="html">My first blog - Comments</title>
+	<subtitle type="html"></subtitle>
+	<link href="http://jacobtheboss.box/index.php?feed/atom/comments" rel="self" type="application/atom+xml" />
+	<link href="http://jacobtheboss.box/index.php?" rel="alternate" type="text/html" title=""/>
+	<updated>2020-07-31T13:43:39+01:00</updated>
+	<author>
+		<name></name>
+	</author>
+	<id>urn:md5:712b39debf120e1908fbef893785729c</id>
+	<generator uri="https://www.dotclear.org/">Dotclear</generator>
+								<entry>
+				<title>Welcome to Dotclear! - jacob</title>
+				<link href="http://jacobtheboss.box/index.php?post/2020/07/31/Welcome-to-Dotclear%21#c1" rel="alternate" type="text/html" title="Welcome to Dotclear! - jacob" />
+				<id>urn:md5:ad834fc4f355099e6d8759a4b5756189</id>
+				<published>2020-07-31T14:43:39+01:00</published>
+				<updated>2020-07-31T14:43:39+01:00</updated>
+				<author>
+					<name>jacob</name>
+				</author>
+				<content type="html">&lt;p&gt;With this new content manager the company will become more dynamic, I'm sure.&lt;/p&gt;</content>
+			</entry>
+			</feed>
---
+++ tryhackme/Jacob the Boss/index.php
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?><feed xmlns="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xml:lang="pt">
+	<title type="html">My first blog</title>
+	<subtitle type="html"></subtitle>
+	<link href="http://jacobtheboss.box/index.php?feed/atom" rel="self" type="application/atom+xml" />
+	<link href="http://jacobtheboss.box/index.php?" rel="alternate" type="text/html" title=""/>
+	<updated>2020-07-31T13:43:39+01:00</updated>
+	<author>
+		<name></name>
+	</author>
+	<id>urn:md5:712b39debf120e1908fbef893785729c</id>
+	<generator uri="https://www.dotclear.org/">Dotclear</generator>
+			<entry>
+			<title>Welcome to Dotclear!</title>
+			<link href="http://jacobtheboss.box/index.php?post/2020/07/31/Welcome-to-Dotclear%21" rel="alternate" type="text/html" title="Welcome to Dotclear!" />
+			<id>urn:md5:ad834fc4f355099e6d8759a4b5756189</id>
+			<published>2020-07-31T10:38:00+01:00</published>
+							<updated>2020-07-31T10:38:35+01:00</updated>
+									<author>
+				<name>Jacob the Boss</name>
+			</author>
+									<content type="html"> &lt;p&gt;This is your first entry. When you're ready to blog, log in to edit or delete it.&lt;/p&gt;</content>
+			
+							<wfw:comment>http://jacobtheboss.box/index.php?post/2020/07/31/Welcome-to-Dotclear%21#comment-form</wfw:comment>
+				<wfw:commentRss>http://jacobtheboss.box/index.php?feed/atom/comments/1</wfw:commentRss>
+					</entry>
+	</feed>
